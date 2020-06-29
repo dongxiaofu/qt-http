515fab76d447fb12422b1f69c170868ded7f4a96
cg <chuganghong@qq.com>
2020-06-29 15:01:07 +0800

    简单的网页浏览功能

---
+++ http/mainwindow.cpp
@@ -0,0 +1,29 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+#include <QTextCodec>
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+
+    manager = new QNetworkAccessManager(this);
+    connect(manager, SIGNAL(finished(QNetworkReply*)),
+    this,SLOT(replyFinished(QNetworkReply*)));
+    manager->get(QNetworkRequest(QUrl("http://www.chugang.net")));
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+void MainWindow::replyFinished(QNetworkReply *reply)
+{
+    QTextCodec *codec = QTextCodec::codecForName("utf8");
+    QString all = codec->toUnicode(reply->readAll());
+    ui->textBrowser->setText(all);
+    reply->deleteLater();
+}
+
